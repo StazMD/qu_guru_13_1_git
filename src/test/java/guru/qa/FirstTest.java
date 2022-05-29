8f89f68d2a69b45fec68b36447abcbb154ac13fd
Stanislav Scurtu <stanislav.scurtu@gmail.com>
2022-05-29 20:02:37 +0300

    Assertion false

--- src/test/java/guru/qa/FirstTest.java
+++ src/test/java/guru/qa/FirstTest.java
@@ -7,12 +7,12 @@ public class FirstTest {
 
     @Test
     void firstTest() {
-        Assertions.assertTrue(6 > 2);
+        Assertions.assertFalse(6 > 2);
     }
     @Test
 
     void secondTest() {
-        Assertions.assertTrue(6 > 1);
+        Assertions.assertFalse(6 > 1);
     }
 
 }
